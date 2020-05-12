258b20ba449e6dd4c13cc36855d61702702348a5
Roza Grigorkiv <roza_grigorkiv@yahoo.com>
2020-05-11 19:22:10 -0600

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Form = (props) => {
+    const { inputs, setInputs } = props;
+
+    const onChange = e => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input onChange={onChange} type="text" name="firstName" />
+            </div>
+            <br />
+            <div>
+                <label htmlFor="lastname">Last Name</label>
+                <input onChange={onChange} type="text" name="lastName" />
+            </div>
+            <br />
+            <div>
+                <label htmlFor="email">Email</label>
+                <input onChange={onChange} type="text" name="email" />
+            </div>
+            <br />
+            <div>
+                <label htmlFor="password">Password</label>
+                <input onChange={onChange} type="text" name="password" />
+            </div>
+            <br />
+            <div>
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input onChange={onChange} type="text" name="confirmpassword" />
+            </div>
+        </form >
+    );
+};
+export default Form;
